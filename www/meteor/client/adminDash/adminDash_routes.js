eb2d158066a75faef0ac1ee24b357a5579d9cb28
JD Wong <jdmswong@gmail.com>
2015-08-24 13:13:04 -0700

    Added admin dashboard, with skeleton email send button
    Squashed commit of the following:
    
    commit ed59989a8a8228f73f61625a6b8a5bebc985b698
    Author: JD Wong <jdmswong@gmail.com>
    Date:   Mon Aug 24 13:08:36 2015 -0700
    
        added emails button to admin dashboard
    
    commit 849da8f0984644b43786bfacc2269a57ac2d313b
    Author: JD Wong <jdmswong@gmail.com>
    Date:   Mon Aug 24 01:53:02 2015 -0700
    
        admin dashboard rejects all non-admins, displaying a customized error message in the console
    
    commit c094993edb4a12658cf295b7fbf1731aef2b275d
    Author: JD Wong <jdmswong@gmail.com>
    Date:   Mon Aug 24 01:07:30 2015 -0700
    
        created skeleton for admin dashboard.  admin-visible menu option leads to placeholder page

---
+++ www/meteor/client/adminDash/adminDash_routes.js
@@ -0,0 +1,34 @@
+angular.module('monarch')
+
+.config(function($stateProvider) {
+
+  $stateProvider
+
+	// Attendees tab
+	.state('menu.adminDash',{
+		url: '/adminDash',
+		views: {
+			'menu': {
+				templateUrl: 'client/adminDash/adminDash.ng.html',
+				controller: 'AdminDashCtrl',
+				resolve: {
+					// resolve a promise before changing states
+					"adminUser": function($meteor){
+						// returns a promise for the user, throws error
+						// if callback doesn't return true.  This error is 
+						// caught by $rootScope.$on("$stateChangeError"
+						return $meteor.requireValidUser(function(user){
+							return Roles.userIsInRole(user, 'admin') ? 
+								true : 'NEED_ADMIN';
+						});
+					}
+				}
+			}
+		}
+	})
+	
+	;
+	
+})
+
+;
